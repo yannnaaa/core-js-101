03f2021238c3457ada2aa99744d126bae67b6ab3
yannnaaa <yannnaaa@mail.ru>
2019-12-24 23:41:19 +0300

    feat: solution to 07-promises-tasks

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -28,8 +28,20 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (typeof isPositiveAnswer === 'boolean') {
+      if (isPositiveAnswer) {
+        resolve('Hooray!!! She said "Yes"!');
+      }
+
+      if (!isPositiveAnswer) {
+        resolve('Oh no, she said "No".');
+      }
+    } else {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+  });
 }
 
 
@@ -48,8 +60,17 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return new Promise((resolve, reject) => {
+    const rez = [];
+    for (let i = 0; i < array.length; i += 1) {
+      (array[i].then((fulfilled) => {
+        rez.push(fulfilled);
+      }));
+    }
+    resolve(rez);
+    reject(new Error());
+  });
 }
 
 /**
